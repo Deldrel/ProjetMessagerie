799ee4d49dcf1e2637532e19b5d8680d8f84cecd
pierre <plapolla9@gmail.com>
2023-04-06 10:25:08 +0200

    premier test connexion client/server

--- client/src/Client.java
+++ client/src/Client.java
@@ -1,5 +1,27 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class Client {
     public static void main(String[] args) {
-        System.out.println("new Client main() called");
+        try (Socket socket = new Socket("localhost", 1234)) {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            Scanner sc = new Scanner(System.in);
+            String line = null;
+
+            while (!"exit".equalsIgnoreCase(line)) {
+                line = sc.nextLine();
+                out.println(line);
+                out.flush();
+                System.out.println("Server replied " + in.readLine());
+            }
+
+            socket.close();
+            sc.close();
+        } catch (IOException e) {
+            System.out.println("Client exception: " + e.getMessage());
+        }
     }
 }
--- server/src/Server.java
+++ server/src/Server.java
@@ -1,5 +1,31 @@
+import java.io.*;
+import java.net.*;
+
 public class Server {
     public static void main(String[] args) {
-        System.out.println("new Server main() called");
+        ServerSocket server = null;
+
+        try {
+            server = new ServerSocket(1234);
+            server.setReuseAddress(true);
+
+            while (true) {
+                Socket client = server.accept();
+                System.out.println("New client connected " + client.getInetAddress().getHostAddress());
+                ClientHandler clientSock = new ClientHandler(client);
+                new Thread(clientSock).start();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Server exception: " + e.getMessage());
+        } finally {
+            if (server != null) {
+                try {
+                    server.close();
+                } catch (IOException e) {
+                    System.out.println("Server exception: " + e.getMessage());
+                }
+            }
+        }
     }
 }
